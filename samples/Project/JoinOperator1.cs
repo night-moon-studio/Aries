8d7bf1389f78bf777abdbccea9195de7ee44b7dd
huxin <16236903+NMSAzulX@users.noreply.github.com>
2020-07-30 17:01:08 +0800

    Create JoinOperator1.cs

---
+++ samples/Project/JoinOperator1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Project
+{
+    public class JoinOperator1<TEntity>
+    {
+
+        public JoinResult<TEntity, TJoinEntity> LefJoin<TJoinEntity>()
+        {
+
+        }
+    }
+
+
+    public class JoinResult<TEntity,TJoinEntity> {
+
+        public JoinOperator1<TEntity> Link;
+        public string TypeName;
+        public void Test()
+        {
+            return WithResult(new {  })
+        }
+        public JoinOperator1<TEntity> WithResult(object instance)
+        {
+            TypeName = instance.GetType().Name;
+            return Link;
+        }
+    
+    }
+
+
+}
