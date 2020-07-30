a698a37c95786e23ca4401b2cad23bad3bb21aca
huxin <16236903+NMSAzulX@users.noreply.github.com>
2020-07-30 14:15:59 +0800

    上传项目框架

---
+++ test/BenchmarkProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BenchmarkRunner.Run<>();
+            Console.ReadKey();
+        }
+    }
+}
