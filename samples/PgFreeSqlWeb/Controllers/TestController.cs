78d35e2f0dbef2c72b1ca86ff568122c6f79059a
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-09-18 09:03:57 +0800

    re

--- samples/PgFreeSqlWeb/Controllers/TestController.cs
+++ samples/PgFreeSqlWeb/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,20 @@ namespace PgFreeSqlWeb.Controllers
         public TestController(IFreeSql freeSql) : base(freeSql)
         {
 
+        }
+
+
+        [HttpPost("join")]
+        public ApiReturnPageResult GetJoinList(SqlModel<Test> sqlModel)
+        {
+            return Result( _freeSql
+                .QueryFromSqlModel(sqlModel,out long total).ToJoinList(item => new
+                {
+                    item.Id,
+                    TestName = item.Name,
+                    DomainName = InnerJoin<Test2>.MapFrom(item => item.Name),
+                    TypeName = InnerJoin<Test3>.MapFrom(item => item.TypeName)
+                }),total);
         }
     }
 }
