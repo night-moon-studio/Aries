e00a0f03bf536e5fc3497aa29b6f6c2ed099dfb9
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-09-18 09:59:21 +0800

    Update Program.cs

--- samples/Project/Program.cs
+++ samples/Project/Program.cs
@@ -60,69 +60,7 @@ namespace Project
             return default;
         }
 
-        //public static IEnumerable<TReturn> Test<TReturn>(Expression<Func<Student, TReturn>> expression)
-        //{
-        //    //if (!SelectFieldsMappping.ContainsKey(typeof(TReturn)))
-        //    //{
-        //    //Link._joinResultCache[typeof(TReturn)] = new Dictionary<string, string>();
-        //    // 获取构造函数参数
-        //    var arguments = ((NewExpression)expression.Body).Arguments;
-        //    //获取匿名类成员
-        //    var members = ((NewExpression)expression.Body).Members;
-
-
-        //    StringBuilder builder = new StringBuilder();
-        //    for (int i = 0; i < arguments.Count; i++)
-        //    {
-        //        // 方法类型参数
-        //        if (arguments[i].NodeType == ExpressionType.Call)
-        //        {
-        //            var methodExpression = (MethodCallExpression)arguments[i];
-        //            var type = methodExpression.Method.DeclaringType;
-        //            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(InnerJoinHelper<>))
-        //            {
-        //                var joinType = type.GetGenericArguments()[0];
-        //                if (methodExpression.Arguments[0].NodeType == ExpressionType.Quote)
-        //                {
-        //                    var quoteExpression = (UnaryExpression)methodExpression.Arguments[0];
-        //                    if (quoteExpression.Operand.NodeType == ExpressionType.Lambda)
-        //                    {
-        //                        var lambdaExpression = (LambdaExpression)quoteExpression.Operand;
-        //                        if (lambdaExpression.Body.NodeType == ExpressionType.MemberAccess)
-        //                        {
-        //                            var memberExpression = (MemberExpression)lambdaExpression.Body;
-        //                            var result = $"{JoinHelper.GetJoinScript(joinType)}.\"{memberExpression.Member.Name}\" as \"{members[i].Name}\",";
-        //                        }
-
-        //                    }
-        //                }
-        //            }
-                    
-        //            //item.Name = Method(xxx);
-        //            //获取 Method(xxx); 的结果
-        //            //var value = NDelegate.DefaultDomain().Func<string>($"return {((MethodCallExpression)bodys[i]).Method.ReflectedType.GetDevelopName()}.{bodys[i]};")();
-        //            ///builder.Append($"{members[i].Name} = \"{value.Replace("\"", "\\\"")}\",");
-        //        }
-
-        //    }
-
-        //    //去掉“，”
-        //    if (builder.Length > 1)
-        //    {
-        //        builder.Length -= 1;
-        //    }
-
-        //    // 闭合
-        //    builder.Insert(0, "return arg.ToList(item=>new {");
-        //    builder.Append("});");
-
-        //    //适用静态类缓存起来
-        //    //JoinObjectCache<ISelect<T>, TReturn>.GetObjects = NDelegate.RandomDomain().Func<ISelect<T>, object>(builder.ToString());
-
-        //    // }
-        //    //TypeName = typeof(TReturn).Name;
-        //    return default;
-        //}
+       
     }
 
 
