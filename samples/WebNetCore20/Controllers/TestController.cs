e89e3c0f8a1603336bd6798fe99edd45da1cfd96
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-09 20:01:38 +0800

    测试2.0案例

---
+++ samples/WebNetCore20/Controllers/TestController.cs
@@ -0,0 +1,91 @@
+﻿using Aries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestLib;
+
+namespace PgFreeSqlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : AriesCurdController<Test>
+    {
+        //AriesOptimisticLock _lock;
+        public TestController(IFreeSql freeSql) : base(freeSql)
+        {
+            //_lock = new AriesOptimisticLock(freeSql);
+            //_lock.SpecifyLock(1,"test");
+        }
+
+        [HttpGet("testLock")]
+        public int TestLock()
+        {
+            Parallel.For(0, 100, (_,state) => {
+
+                AriesOptimisticLock _lock = new AriesOptimisticLock(_freeSql);
+                _lock.SpecifyLock(uid:1, name:"test");
+                _lock.Execute(() =>
+                {
+                    //var score =  _freeSql.Select<TestLock>().First();
+                    //System.Diagnostics.Debug.WriteLine("TEST:当前分数\t" + score.Score);
+                    //_freeSql.Ado.ExecuteNonQuery("UPDATE public.\"TestLock\" SET \"Score\" = \"Score\" + 1 where \"Score\"=" + score.Score);
+
+                    System.Diagnostics.Debug.WriteLine("进入函数");
+                    _freeSql.Ado.ExecuteNonQuery("UPDATE public.\"TestLock\" SET \"Score\" = \"Score\" + 1");
+
+                });
+
+            });
+            return 0;
+            // });
+        }
+
+        public override ApiReturnResult QuerySingle([FromBody] SqlModel<Test> query)
+        {
+            query.AddQueryCondition(item => item.Id == 1);
+            return base.QuerySingle(query);
+        }
+
+
+        public override ApiReturnResult Modify([FromBody] SqlModel<Test> opModel)
+        {
+            opModel.ModifyInstance.Instance.Name = "test";
+            opModel.AddModifyField(item => item.Name);
+            return base.Modify(opModel);
+        }
+
+        [HttpDelete]
+        public ApiReturnResult Test()
+        {
+
+            return Result(_freeSql.Select<Test>().ToJoinList(item => new {
+                TestName = item.Name,
+                DomainId = item.Domain.AriesInnerJoin<Test21>(c => c.Id).Id,
+                DomainName = item.Domain.AriesInnerJoin<Test21>(c => c.Id).Name,
+                TypeName = item.Type.AriesInnerJoin<Test21>(c => c.Id).Name
+            }));
+
+        }
+
+        //[HttpPost("join")]
+        //public ApiReturnPageResult GetJoinList(SqlModel<Test> sqlModel)
+        //{
+        //    //return Result( _freeSql
+        //    //    .QueryFromSqlModel(sqlModel,out long total).ToJoinList(item => new
+        //    //    {
+        //    //        item.Id,
+        //    //        TestName = item.Name,
+        //    //        DomainName = InnerJoin<Test2>.MapFrom(item => item.Name),
+        //    //        TypeName = InnerJoin<Test3>.MapFrom(item => item.TypeName)
+        //    //    }),total);
+        //}
+    }
+
+    public class Student
+    {
+        public string Name;
+        public string Age;
+        public string Address;
+    }
+
+
+}
---
+++ samples/WebNetCore20/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebNetCore20
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAriesPgSql(
+                "Host=127.0.0.1;Port=5432;Username=postgres;Password=123456; Database=test;Pooling=true;Minimum Pool Size=1",
+                freeSql => {
+
+                    freeSql.Aop.CurdBefore += Aop_CurdBefore;
+
+                });
+
+            //初始化扫描
+            //services.AddAriesEntities(typeof(Test),typeof(Test21));
+            services.AddAriesAssembly("TestLib");
+
+
+            //配置 Join 关系
+            //OrmNavigate<Test>.Connect<Test2>(test => test.Domain, test2 => test2.Id);
+            //OrmNavigate<Test>.Connect<Test3>(test => test.Type, test3 => test3.Id);
+            //OrmNavigate<Test>.Join<Test3>("Type", "Id");
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                //c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, SwaggerConfiguration.XmlName));
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
